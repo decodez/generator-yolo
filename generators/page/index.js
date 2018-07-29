691473d4d860371658b66b6ac035783a2d5e5f6d
Akhil Prasenan <akhil_prasenan@outlook.com>
2018-07-29 19:43:36 +0800

    removed commented out codes

--- generators/page/index.js
+++ generators/page/index.js
@@ -15,29 +15,5 @@ module.exports = class extends Generator {
         );
     }
 
-//   writing() {
-//     this.fs.copyTpl(
-//       this.templatePath('_package.json'),
-//       this.destinationPath('package.json'), {
-//         name: this.props.name,
-//         authorName: this.props.authorName
-//       }
-//     );
-//     this.fs.copyTpl(
-//       this.templatePath('_postcss.config.js'),
-//       this.destinationPath('postcss.config.js')
-//     );
-//     this.fs.copyTpl(
-//       this.templatePath('_webpack.config.js'),
-//       this.destinationPath('webpack.config.js')
-//     );
-//     this.fs.copyTpl(
-//       this.templatePath('src'),
-//       this.destinationPath('src'),{
-//         name: this.props.name
-//       }
-//     );
-//   }
-
 
 };
