0106f33657505733a168057f6061172bc41f7b18
akhil.prasenan <akhil.prasenan@adelphi.digital>
2018-07-30 17:33:01 +0800

    updated to generate proper class name

--- generators/module/index.js
+++ generators/module/index.js
@@ -1,46 +1,62 @@
 'use strict';
 const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
-
-
+const _ = require('lodash');
 
 module.exports = class extends Generator {
+  writing() {
+    let convertedClassName = _.camelCase(this.arguments);
+    convertedClassName = _.upperFirst(convertedClassName);
 
-
-
-
-    writing() {
-
-        if (this.options.atomic) {
-            this.fs.copyTpl(
-                this.templatePath('module.pug'),
-                this.destinationPath(`src/_modules/${this.options.atomic}s/${this.arguments}/${this.arguments}.pug`),{
-                    name: this.arguments
-                }
-            );
-            this.fs.copyTpl(
-                this.templatePath('module.scss'),
-                this.destinationPath(`src/_modules/${this.options.atomic}s/${this.arguments}/${this.arguments}.scss`),{
-                    name: this.arguments
-                }
-            );
-            this.fs.copyTpl(
-                this.templatePath('module.js'),
-                this.destinationPath(`src/_modules/${this.options.atomic}s/${this.arguments}/${this.arguments}.js`),{
-                    name: this.arguments
-                }
-            );
-        } else {
-            this.fs.copyTpl(
-                this.templatePath('module.pug'),
-                this.destinationPath(`src/_modules/${this.arguments}/${this.arguments}.pug`),{
-                    name: this.arguments
-                }
-            );
+    if (this.options.atomic) {
+      this.fs.copyTpl(
+        this.templatePath('module.pug'),
+        this.destinationPath(
+          `src/_modules/${this.options.atomic}s/${this.arguments}/${this.arguments}.pug`
+        ),
+        {
+          name: this.arguments
         }
-        
+      );
+      this.fs.copyTpl(
+        this.templatePath('module.scss'),
+        this.destinationPath(
+          `src/_modules/${this.options.atomic}s/${this.arguments}/${this.arguments}.scss`
+        ),
+        {
+          name: this.arguments
+        }
+      );
+      this.fs.copyTpl(
+        this.templatePath('module.js'),
+        this.destinationPath(
+          `src/_modules/${this.options.atomic}s/${this.arguments}/${this.arguments}.js`
+        ),
+        {
+          name: convertedClassName
+        }
+      );
+    } else {
+      this.fs.copyTpl(
+        this.templatePath('module.pug'),
+        this.destinationPath(`src/_modules/${this.arguments}/${this.arguments}.pug`),
+        {
+          name: this.arguments
+        }
+      );
+      this.fs.copyTpl(
+        this.templatePath('module.scss'),
+        this.destinationPath(`src/_modules/${this.arguments}/${this.arguments}.scss`),
+        {
+          name: this.arguments
+        }
+      );
+      this.fs.copyTpl(
+        this.templatePath('module.js'),
+        this.destinationPath(`src/_modules/${this.arguments}/${this.arguments}.js`),
+        {
+          name: convertedClassName
+        }
+      );
     }
-
-
+  }
 };
