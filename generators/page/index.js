25a9b6d276629ca59bd14f8fd98496d9832a5192
akhil.prasenan <akhil.prasenan@adelphi.digital>
2018-07-30 18:14:00 +0800

    Added function to automatically add new html page entries in the webpack config file

--- generators/page/index.js
+++ generators/page/index.js
@@ -3,12 +3,29 @@ const Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
   writing() {
+    let pages = this.config.get('pages');
+
+    if (pages === undefined) {
+      pages = [];
+      pages.push(this.arguments[0]);
+    } else {
+      pages.push(this.arguments[0]);
+    }
+    this.config.set('pages', pages);
+
     this.fs.copyTpl(
-      this.pages.push(this.arguments),
       this.templatePath('page.pug'),
       this.destinationPath(`src/${this.arguments}/index.pug`),
       {
         name: this.arguments
+      }
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('_webpack.config.js'),
+      this.destinationPath(`webpack.config.js`),
+      {
+        pages: pages
       }
     );
   }
