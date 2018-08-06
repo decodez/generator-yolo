28faa52573eacf4407d8cdac3080150e28e29785
decodez <akhil_prasenan@outlook.com>
2018-08-06 17:37:24 +0800

    Updated module generator to check for empty entries and also wrong atomic entries

--- generators/module/index.js
+++ generators/module/index.js
@@ -1,5 +1,6 @@
 'use strict';
 const Generator = require('yeoman-generator');
+const chalk = require('chalk');
 const _ = require('lodash');
 
 module.exports = class extends Generator {
@@ -13,33 +14,47 @@ module.exports = class extends Generator {
     convertedClassName = _.upperFirst(convertedClassName);
 
     if (this.options.atomic) {
-      this.fs.copyTpl(
-        this.templatePath('module.pug'),
-        this.destinationPath(
-          `src/_modules/${this.options.atomic}s/${modulePath}/${moduleName}.pug`
-        ),
-        {
-          name: moduleName
-        }
-      );
-      this.fs.copyTpl(
-        this.templatePath('module.scss'),
-        this.destinationPath(
-          `src/_modules/${this.options.atomic}s/${modulePath}/${moduleName}.scss`
-        ),
-        {
-          name: moduleName
-        }
-      );
-      this.fs.copyTpl(
-        this.templatePath('module.js'),
-        this.destinationPath(
-          `src/_modules/${this.options.atomic}s/${modulePath}/${moduleName}.js`
-        ),
-        {
-          name: convertedClassName
-        }
-      );
+      if (
+        this.options.atomic === 'atom' ||
+        this.options.atomic === 'molecule' ||
+        this.options.atomic === 'organism'
+      ) {
+        this.fs.copyTpl(
+          this.templatePath('module.pug'),
+          this.destinationPath(
+            `src/_modules/${this.options.atomic}s/${modulePath}/${moduleName}.pug`
+          ),
+          {
+            name: moduleName
+          }
+        );
+        this.fs.copyTpl(
+          this.templatePath('module.scss'),
+          this.destinationPath(
+            `src/_modules/${this.options.atomic}s/${modulePath}/${moduleName}.scss`
+          ),
+          {
+            name: moduleName
+          }
+        );
+        this.fs.copyTpl(
+          this.templatePath('module.js'),
+          this.destinationPath(
+            `src/_modules/${this.options.atomic}s/${modulePath}/${moduleName}.js`
+          ),
+          {
+            name: convertedClassName
+          }
+        );
+      } else {
+        this.log(
+          chalk.red(
+            'Invalid input! atomic flag will only recieve 3 options, "atom","molecule" or "organism"'
+          )
+        );
+      }
+    } else if (this.arguments.length === 0) {
+      this.log(chalk.red('No module name entered. Please enter a module name.'));
     } else {
       this.fs.copyTpl(
         this.templatePath('module.pug'),
